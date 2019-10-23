749277975e271f036a8f184033f4d222c68e86ad
Nuzha Rukiya <r.nuzha@gmail.com>
2019-10-23 19:32:50 +0530

    Caching network calls in Retrofit

--- app/src/main/java/com/nuzharukiya/baseapplication/connector/OkHttpClientInstance.kt
+++ app/src/main/java/com/nuzharukiya/baseapplication/connector/OkHttpClientInstance.kt
@@ -1,8 +1,11 @@
 package com.nuzharukiya.baseapplication.connector
 
+import android.content.Context
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import timber.log.Timber
+import java.io.File
 
 /**
  * Created by Nuzha Rukiya on 19/10/23.
@@ -10,17 +13,19 @@ import timber.log.Timber
 
 class OkHttpClientInstance {
     companion object {
-        var instance: OkHttpClient? = null
-            get() {
-                if (field == null) {
-                    field = createOkHttpClientInstance()
-                }
-                return field
-            }
+        private var instance: OkHttpClient? = null
 
-        private fun createOkHttpClientInstance(): OkHttpClient? {
+        fun getInstance(mContext: Context): OkHttpClient? {
+            if (instance == null) {
+                instance = createOkHttpClientInstance(mContext)
+            }
+            return instance
+        }
+
+        private fun createOkHttpClientInstance(mContext: Context): OkHttpClient? {
             return OkHttpClient.Builder()
                 .addInterceptor(getHttpLoggingInterceptor())
+                .cache(getCache(mContext))
                 .build()
         }
 
@@ -31,6 +36,13 @@ class OkHttpClientInstance {
                     Timber.i(message)
                 }
             })
+        }
+
+        private fun getCache(mContext: Context): Cache {
+            val cacheFile = File(mContext.cacheDir, "okhttp_cache")
+            cacheFile.mkdirs()
+
+            return Cache(cacheFile, 10 * 10 * 1000)
         }
     }
 }
